907d7816d2ba80756b64d70eddf3b826ccda57c8
Dan Granados <dfgranadosreyes@gmail.com>
2014-10-08 23:02:30 -0400

    linked teacher names to the id of the teacher teaching each class, as well as pictures. fixed search functionality for left menu

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-var app = angular.module('seedApp.controllers', ['classDataService', 'dateTimeService']);
+var app = angular.module('seedApp.controllers', ['classDataService', 'dateTimeService', 'personDataService']);
 
 app.controller('NavCtrl', function($scope, $ionicSideMenuDelegate) {
   $scope.showMenu = function () {
@@ -14,31 +14,34 @@ app.controller('HomeTabCtrl', function($scope) {
 
 
 
-app.controller('ClassesTabCtrl', function($scope, classesFactory, dateTimeFactory) {
+app.controller('ClassesTabCtrl', function($scope, classesFactory, dateTimeFactory, personFactory) {
   $scope.yClass = classesFactory.all();
 
-  $scope.dayAsText = dateTimeFactory.dayAsText;
-
-  $scope.currentMonth = dateTimeFactory.currentMonth;
-
-  $scope.currentDay = dateTimeFactory.currentDay;
-
-  $scope.mineVar = '';
-
-  $scope.checkClick = function(n)
-  {
-    console.log('clicked');
-  };
-
-$scope.classSearchFilter = function (yClass) {
+    $scope.classSearchFilter = function (yClass) {
+      teacherName = $scope.getClassTeacher(yClass.id);
     var keyword = new RegExp($scope.classFilter, 'i');
-    return !$scope.classFilter || keyword.test(yClass.location) || keyword.test(yClass.yogaType) || keyword.test(yClass.teacher) ;
-};
+        return !$scope.classFilter || keyword.test(teacherName) || keyword.test(yClass.location);
+    };
 
-  $scope.repeatQ = function(n)
-  {
-    return new Array(n);
-  };
+    $scope.getClassTeacher = function(teacherId){
+      teacher = personFactory.get(teacherId);
+      return teacher.Person.firstName + " " + teacher.Person.lastName;
+    };
+
+      $scope.dayAsText = dateTimeFactory.dayAsText;
+
+      $scope.currentMonth = dateTimeFactory.currentMonth;
+
+      $scope.currentDay = dateTimeFactory.currentDay;
+
+      $scope.repeatQ = function(n)
+      {
+        return new Array(n);
+      };
+});
+
+app.controller('MapTabCtrl', function($scope) {
+
 });
 
 app.controller('MapTabCtrl', function($scope) {
@@ -56,43 +59,12 @@ app.directive("menu-icon", function () {
 });
 
 
-app.controller('searchController', function($scope) {
-    $scope.personList = [
-      {
-          Person: {
-              firstName: 'Paula',
-              familyName: 'Pavlova'
-          },
-          location: "DC",
-          Constructors: [
-              {name: "teacher"}
-          ]
-      },
-       {
-          Person: {
-              firstName: 'Dan',
-              familyName: 'Granados'
-          },
-          location: "Austin",
-          Constructors: [
-              {name: "student"}
-          ]
-      },
-      {
-          Person: {
-          firstName: 'Diplo',
-              familyName: 'Pavlov'
-          },
-          location: "LA",
-          Constructors: [
-              {name: "puppy"}
-          ]
-      }
-    ];
-    $scope.searchFilter = function (person) {
-    var keyword = new RegExp($scope.nameFilter, 'i');
-    return !$scope.nameFilter || keyword.test(person.Person.givenName) || keyword.test(person.Person.familyName);
-};
+app.controller('searchController', function($scope, personFactory) {
+        $scope.personList = personFactory.all();
+        $scope.searchFilter = function (person) {
+        var keyword = new RegExp($scope.nameFilter, 'i');
+        return !$scope.nameFilter || keyword.test(person.Person.firstName) || keyword.test(person.Person.lastName);
+    };
 });
 
 
--- www/js/services.js
+++ www/js/services.js
@@ -6,16 +6,16 @@ angular.module('classDataService', [])
 
   // Some fake testing data
   var classes = [
-    { id: 0, yogaType: 'Hot Yoga', teacher: 'Paula Pavlova', time:'6 pm' , location:'Adam\'s Morgan', year:"2014", month:"10" , day:8, mine:'y' },
-    { id: 1, yogaType: 'Ashtanga Yoga', teacher: 'Diplo Weasly', time: '8 pm' , location:'Dupont' , year:"2014", month:"10" , day:8, mine:'n'  },
-    { id: 2, yogaType: 'Power Yoga', teacher: 'Suzzie Suzzington', time: '9 pm' , location:'Tenley' , year:"2014", month:"10" , day:8, mine:'n'  },
-     { id: 3, yogaType: 'Power Yoga', teacher: 'Timmy Timmington', time: '9 am' , location:'H Street' , year:"2014", month:"12" , day:1, mine:'y'  },
-    { id: 4, yogaType: 'Kundalini Yoga', teacher: 'Andy Anditon', time: '12 pm' , location:'Shaw' , year:"2014", month:"11" , day:1, mine:'n'  },
-    { id: 5, yogaType: 'Hot Yoga', teacher: 'Paula Pavlova', time: '3 pm', location:'Columbia Heights' , year:"2014", month:"10" , day:9, mine:'y'  },
-    { id: 6, yogaType: 'Ashtanga Yoga', teacher: 'Diplo Weasly', time: '8 am' , location:'Rosslyn' , year:"2014", month:"10" , day:10, mine:'n'  },
-    { id: 7, yogaType: 'Power Yoga', teacher: 'Suzzie Suzzington', time: '10am' , location:'DaSpot' , year:"2014", month:"10" , day:10, mine:'n'  },
-     { id: 8, yogaType: 'Power Yoga', teacher: 'Timmy Timmington', time: '4:30 pm' , location:'DaPlace' , year:"2014", month:"10" , day:10, mine:'y'  },
-    { id: 9, yogaType: 'Kundalini Yoga', teacher: 'Andy Anditon', time: '5pm', location:'DaLocation', year:"2014", month:"10" , day:10, mine:'y'   }
+    { id: 0, yogaType: 'Hot Yoga', teacherId: '0', time:'6 pm' , location:'Adam\'s Morgan', year:"2014", month:"10" , day:8, mine:'y' },
+    { id: 1, yogaType: 'Ashtanga Yoga', teacherId: '1', time: '8 pm' , location:'Dupont' , year:"2014", month:"10" , day:8, mine:'n'  },
+    { id: 2, yogaType: 'Power Yoga', teacherId: '2', time: '9 pm' , location:'Tenley' , year:"2014", month:"10" , day:8, mine:'n'  },
+     { id: 2, yogaType: 'Power Yoga', teacherId: '3', time: '9 am' , location:'H Street' , year:"2014", month:"12" , day:1, mine:'y'  },
+    { id: 4, yogaType: 'Kundalini Yoga', teacherId: '4', time: '12 pm' , location:'Shaw' , year:"2014", month:"11" , day:1, mine:'n'  },
+    { id: 5, yogaType: 'Hot Yoga', teacherId: '9', time: '3 pm', location:'Columbia Heights' , year:"2014", month:"10" , day:9, mine:'y'  },
+    { id: 6, yogaType: 'Ashtanga Yoga', teacherId: '1', time: '8 am' , location:'Rosslyn' , year:"2014", month:"10" , day:10, mine:'n'  },
+    { id: 7, yogaType: 'Power Yoga', teacherId: '7', time: '10am' , location:'DaSpot' , year:"2014", month:"10" , day:10, mine:'n'  },
+     { id: 8, yogaType: 'Power Yoga', teacherId: '3', time: '4:30 pm' , location:'DaPlace' , year:"2014", month:"10" , day:10, mine:'y'  },
+    { id: 9, yogaType: 'Kundalini Yoga', teacherId: '4', time: '5pm', location:'DaLocation', year:"2014", month:"10" , day:10, mine:'y'   }
   ];
 
   return {
@@ -35,26 +35,27 @@ angular.module('personDataService', [])
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var classes = [
-    { id: 0, firstName:"Paula", lastName:"Pavlova", location: "DC", style: "Vinyasa" },
-    { id: 1, yogaType: 'Ashtanga Yoga', teacher: 'Diplo Weasly', length: '60 min'  },
-    { id: 2, yogaType: 'Power Yoga', teacher: 'Suzzie Suzzington', length: '75 min'  },
-     { id: 3, yogaType: 'Power Yoga', teacher: 'Timmy Timmington', length: '90 min'  },
-    { id: 4, yogaType: 'Kundalini Yoga', teacher: 'Andy Anditon', length: '75 min'  },
-    { id: 5, yogaType: 'Hot Yoga', teacher: 'Paula Pavlova', length: '45 min' },
-    { id: 6, yogaType: 'Ashtanga Yoga', teacher: 'Diplo Weasly', length: '60 min'  },
-    { id: 7, yogaType: 'Power Yoga', teacher: 'Suzzie Suzzington', length: '75 min'  },
-     { id: 8, yogaType: 'Power Yoga', teacher: 'Timmy Timmington', length: '90 min'  },
-    { id: 9, yogaType: 'Kundalini Yoga', teacher: 'Andy Anditon', length: '75 min'  }
+  var people = [
+    { id: 0, Person: {firstName:'Paula', lastName:'Pavlova'}, location: "DC"},
+    { id: 1, Person: {firstName: 'Diplo', lastName: ' Weasly'}, location: 'Austin'  },
+    { id: 2, Person: {firstName: 'Suzzie', lastName: ' Suzzington'}, location: 'LA'  },
+     { id: 3, Person: {firstName: 'Timmy', lastName: ' Timmeington'}, location: 'DC'  },
+    { id: 4, Person: {firstName: 'Andy', lastName: ' Anditon'}, location: 'DC'  },
+    { id: 5, Person: {firstName: 'James', lastName: ' Peach'}, location: 'LA' },
+    { id: 6, Person: {firstName: 'Joe', lastName: ' Apple'}, location: 'DC'  },
+    { id: 7, Person: {firstName: 'Leonard', lastName: ' MiLeonard'}, location: 'Austin'  },
+     { id: 8, Person: {firstName: 'Becky', lastName: ' Banana'}, location: 'DC'  },
+    { id: 9, Person: {firstName: 'Jane', lastName: ' Juice'}, location: 'Austin'  },
+    { id: 10, Person: {firstName: 'The', lastName: ' Dude'}, location: 'NY'  }
   ];
 
   return {
     all: function() {
-      return classes;
+      return people;
     },
-    get: function(classId) {
+    get: function(personId) {
       // Simple index lookup
-      return classes[classId];
+      return people[personId];
     }
   };
 });
